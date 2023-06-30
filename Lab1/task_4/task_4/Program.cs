14563ee190bf436b1e063975b10f6a0b5bb395be
Tisary <antonukden83@gmail.com>
2023-06-30 15:24:11 +0300

    labs

---
+++ Lab1/task_4/task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿int x1 = int.Parse(Console.ReadLine());
+int x2 =int.Parse(Console.ReadLine());
+int x3 =int.Parse(Console.ReadLine());
+int x4 = int.Parse(Console.ReadLine());
+
+if (x1 == x4 && x1 == 0)
+{
+    Console.WriteLine(true);
+}
+if (x1 == x2 && x2==x3 && x1 == 1)
+{
+    Console.WriteLine(true);
+}
+if (x1 == x4 && x3==0 && x1 == 1)
+{
+    Console.WriteLine(true);
+}
